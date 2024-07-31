5a567931040d26c10ba7cfee57d1fca2f5ad4bc8
MalshiTmel <malshi.github@gmail.com>
2024-07-31 18:22:44 +0530

    updated

--- models/Route.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const RouteSchema = new Schema({
-  name: String,
-  stations: [String]
-});
-
-module.exports = mongoose.model('Route', RouteSchema);
